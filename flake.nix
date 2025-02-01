b183724a40745768ad3be54d8a51d4f433263c77
achref <achrefbenmbarek123@gmail.com>
2025-02-01 11:17:50 +0100

    added a flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "ko environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = { self, nixpkgs }: 
+  let
+  system = "x86_64-linux";
+  pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    devShells.x86_64-linux.kuber = pkgs.mkShell  {
+      buildInputs = with pkgs; [ 
+      ko
+];
+
+    shellHook = ''
+            if [ -f ~/.zshrc ]; then
+              zsh
+            elif [ -f ~/.bashrc ]; then
+              bash
+            fi
+          '';
+    };
+  };
+}
+
+
+
